c23859eb601cca18db7fc2c43cd7a6c1e9357914
Teet Simson <teet5858@gmail.com>
2022-03-26 20:43:41 +0000

    Full name fix

--- Scripts/validate.js
+++ Scripts/validate.js
@@ -2,21 +2,21 @@ function validateForm() {
     
     let name = document.forms["DemoForm"]["full-name"];
     let message = document.forms["DemoForm"]["message"];
-    console.log(message.value);
-    if (name.value.split(" ").length < 2) {
-        alert("Please enter your full name.");
+
+    if (name.value.split(" ").length < 2 || name.value.split(" ")[1].trim().length === 0) {
+        alert("Please enter your full name!");
         name.focus();
         return false;
     }
 
     if (message.value.length < 8) {
-        alert("Message too short");
+        alert("Message too short!");
         message.focus();
         return false;
     }
 
     if (message.value.split(" ")[0].length > 20) {
-        alert("Invalid message");
+        alert("Invalid message!");
         message.focus();
         return false;
     }
